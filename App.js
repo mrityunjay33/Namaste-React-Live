1b25e006657b372dae1e79e2baeab8db2ee25062
Mrityunjay Kumar <mk10399@gmail.com>
2023-02-19 17:15:08 +0530

    Day3 - Verion 1

--- App.js
+++ App.js
@@ -16,6 +16,7 @@
  * port Number
  * Consistent Hashing Algorithm
  * Zero Config
+ * Tree Shacking -> Removing unwanted Codes
  *
  *
  *
@@ -29,6 +30,7 @@ const heading = React.createElement(
   "h1",
   {
     id: "title",
+    key: "h1",
   },
   "Heading 1 for parcel"
 );
@@ -37,6 +39,7 @@ const heading2 = React.createElement(
   "h2",
   {
     id: "title",
+    key: "h2",
   },
   "Heading 2"
 );
