6d296171594461d58608700bd0769c002dd2a901
Mrityunjay Kumar <mk10399@gmail.com>
2023-02-19 19:02:15 +0530

    React element inside React Functional Component

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = (     //This is React element => It is object at EOD (heading is normal js variable)
+const heading = ( 
   <h1 id="title" key="h1">  
     Namaste React
   </h1>
 );
-/**React Components
- *  - Functional => NEW way of writing code   -> Will do mostly this
- *  - Class Based Component => OLD way
- * 
- *  Name of component starts with Capital letter - It's not mandatory
- */
 
-const HeaderComponent = () => {   //This is React Functional Component  => It is Funtion at EOD (HeaderComponent is normal js function)
+const HeaderComponent = () => {   
   return (
     <div>
+      {heading}
       <h1> Namaste React Functional Component </h1>
       <h2> This is h2 tag </h2>
     </div>
@@ -25,4 +20,4 @@ const HeaderComponent = () => {   //This is React Functional Component  => It is
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeaderComponent/>);  //for rendering react element 'root.render(heading);'
+root.render(<HeaderComponent/>);  
