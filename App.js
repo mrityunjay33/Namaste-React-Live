61223b6645d3ae859fc49fb47652bfa60f5eb232
Mrityunjay Kumar <mk10399@gmail.com>
2023-02-19 18:54:29 +0530

    Functional Component basics

--- App.js
+++ App.js
@@ -1,56 +1,34 @@
-/***
- *
- * Created A Server
- * HMR - Hot Module Replacement
- * File Watcher algorithm - C++
- * BUNDLING
- * MINIFY
- * Cleaning our Code
- * Dev abd Production Build
- * Super Fast build algorithm
- * Image Optimization
- * Caching while development
- * Compression
- * Compatble with older version of browser
- * HTTPS on dev
- * port Number
- * Consistent Hashing Algorithm
- * Zero Config
- * Tree Shacking -> Removing unwanted Codes
- *
- *
- *
- * Transitive Dependencies
- */
-
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-// React.createElement => Object => HTML(DOM)
-
-//  JSX ???     (Babel converts JSX to React.createElement)
-/**
- *  Advantages of JSX
- *  1. Developers Experience Better
- *  2. Syntactical Sugar
- *  3. Readability
- *  4. Less Code
- *  5. Maintainability
- *  6. No Repetation
- */
-// JSX => React.createElement => Object => HTML(DOM)
 
 const heading = (
   <h1 id="title" key="h1">
     Namaste React
-    </h1>
-  );
+  </h1>
+);
+/**React Components
+ *  - Functional => NEW way of writing code   -> Will do mostly this
+ *  - Class Based Component => OLD way
+ * 
+ *  Name of component starts with Capital letter - It's not mandatory
+ */
 
+const HeaderComponent = () => {
+  return (
+    <div>
+      <h1> Namaste React Functional Component </h1>
+      <h2> This is h2 tag </h2>
+    </div>
+  )
+};
+const HeaderComponent2 = () => {
+    <div>
+      <h1> Namaste React Functional Component </h1>
+      <h2> This is h2 tag </h2>
+    </div>
+};
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-//passing a react element inside the root
-
-//async defer
 root.render(heading);
