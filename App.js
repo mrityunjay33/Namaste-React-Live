f5c1f1307606ed00637efd7d194626e768ed9f3d
Mrityunjay Kumar <mk10399@gmail.com>
2023-02-20 00:15:53 +0530

    Day3 Recap

--- App.js
+++ App.js
@@ -1,24 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+/**
+ * Header
+ *  -Logo
+ *  - Nav Items(Right Side)
+ *  - Cart
+ * Body
+ *  - Search Bar
+ *  - Restaurant List
+ *     - Image
+ *     - Name
+ *     - Cuisines
+ *     - Rating
+ *  - Footer
+ *     - Links
+ *     - Copyrights
+ *
+ */
 
-const Title = () => ( 
-  <h1 id="title" key="h1">  
-    Namaste React
+const Title = () => (
+  <h1 id="title" key="h1">
+    Food Villa
   </h1>
 );
-
-const HeaderComponent = () => {   
+// Polyfills is for older version of browser
+// Babel is JSX cpmpiler/transpiler
+const HeaderComponent = () => {
   return (
-    <div>
+    <div className="header">
       <Title />
-      {Title()}
-      <h1> Namaste React Functional Component </h1>
-      <h2> This is h2 tag </h2>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 };
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeaderComponent/>);  
+root.render(<HeaderComponent />);
