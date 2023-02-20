79504fd211e0287bcc842a5056457f8f818b3fd0
Mrityunjay Kumar <mk10399@gmail.com>
2023-02-20 14:08:04 +0530

    Different ways to import

--- src/App.js
+++ src/App.js
@@ -1,49 +1,15 @@
 import React from "react";
-import ReactDOM, { createRoot } from "react-dom/client";
-/**
-     Header
-        - Logo(Title)
-        - Nav Items(Right Side)
-        - Cart
-     Body 
-        - Search bar
-        - RestrauntList
-          - RestaurantCard (many cards)
-              - Image
-              - Name
-              - Rating
-              - Cusines
-     Footer
-      - links
-      - Copyright
-    
-    */
-const Title = () => (
-  <a href="/">
-    <img
-      className="logo"
-      alt="logo"
-      src="https://yt3.ggpht.com/ytc/AMLnZu_EC-ECXAxRAixWGEfMsE1rdSoetBHyxmLNdtCB=s900-c-k-c0x00ffffff-no-rj"
-    />
-  </a>
-);
+import ReactDOM from "react-dom/client";
 
-// Composing Comopnentss
-const Header = () => {
-  return (
-    <div className="header">
-      <Title />
-      <div className="nav-items">
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-          <li>Cart</li>
-        </ul>
-      </div>
-    </div>
-  );
-};
+// Default Import
+import Title from "./components/Title";
+// Named Import
+import {Header} from "./components/Title";
+
+// Import all    => <Obj.Header /> and <Obj.Title />
+import * as Obj from "./components/Title"
+
+
 
 //Config Driven UI
 
@@ -818,7 +784,7 @@ const Footer = () => {
 const AppLayout = () => {
   return (
     <>
-      <Header />
+      <Obj.Header />
       <Body />
       <Footer />
     </>
