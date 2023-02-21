fc5bcb801c1d1e4748276439fd1703498dbf768c
Mrityunjay Kumar <mk10399@gmail.com>
2023-02-21 17:49:59 +0530

    destructure on the fly

--- src/components/RestraurantMenu.js
+++ src/components/RestraurantMenu.js
@@ -1,9 +1,7 @@
 import { useParams } from "react-router-dom";
 
 const RestraurantMenu = () => {
-    const params = useParams();
-    const {id} = params;
-    console.log(params);
+    const {id} = useParams(); // destructure on the fly
     return (
         <div>
             <h1>Restaurant Menu</h1>
