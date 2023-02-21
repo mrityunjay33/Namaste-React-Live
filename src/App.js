a3e75620e4301b9b3595d5abf72f7ab360b8547d
Mrityunjay Kumar <mk10399@gmail.com>
2023-02-21 17:29:27 +0530

    react router dom -> Link, browserRouter, outlet,

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Body from "./components/Body";
 import About from "./components/About";
+import Contact from "./components/Contact";
+import Cart from "./components/Cart";
 import Error from "./components/Error";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 
 const AppLayout = () => {
   return (
     <>
       <Header />
-      <Body />
+      {/* All the childrens will go into the outlet according to the route */}
+      <Outlet />
       <Footer />
     </>
   );
@@ -21,11 +24,13 @@ const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
-    errorElement: <Error/>,
-  },
-  {
-    path: "/about",
-    element: <About />,
+    errorElement: <Error />,
+    children: [
+      { path: "/", element: <Body /> },
+      { path: "/about", element: <About /> },
+      { path: "/contact", element: <Contact /> },
+      { path: "/cart", element: <Cart /> },
+    ],
   },
 ]);
 
--- src/components/Error.js
+++ src/components/Error.js
@@ -2,12 +2,13 @@ import { useRouteError } from "react-router-dom";
 
 const Error = () => {
     const err = useRouteError();
+    const {status, statusText} = err;
     console.log(err);
     return (
         <>
             <h1>Oops !!</h1>
             <h2>Something went wrong</h2>
-            <p>{err.status + " : " + err.statusText} </p>
+            <p>{status + " : " + statusText} </p>
         </>
     );
 };
