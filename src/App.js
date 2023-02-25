88c6d7bef92e5f9b6cbca8d0b2c6b6248aeb0ed2
Mrityunjay Kumar <mk10399@gmail.com>
2023-02-25 21:21:41 +0530

    Custom Accordion

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from "react";
+import React, { lazy, Suspense, useState } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -16,6 +16,10 @@ const Instamart = lazy(() => import("./components/Instamart"));
 const AboutClass = lazy(() => import("./components/AboutClass"));
 
 const AppLayout = () => {
+  const [user, setUser] = useState({
+    name: "Mrityunjay",
+    email: "Mrityunjay@gmail.com",
+  });
   return (
     <>
       <Header />
--- src/components/Instamart.js
+++ src/components/Instamart.js
@@ -1,9 +1,65 @@
+import { useState } from "react";
+
+const Section = ({ title, description, show, setShow }) => {
+  return (
+    <div className="border border-black p-2 m-2">
+      <h1 className="font-bold text-xl">{title}</h1>
+      {show ? (
+        <button
+          onClick={() => setShow(false)}
+          className="cursor-pointer underline m-2"
+        >
+          hide
+        </button>
+      ) : (
+        <button
+          onClick={() => setShow(true)}
+          className="cursor-pointer underline m-2"
+        >
+          show
+        </button>
+      )}
+
+      {show && <p>{description}</p>}
+    </div>
+  );
+};
+
 const Instamart = () => {
-    return (
-        <>
-        <h1>Instamart</h1>
-        <h2>100s of Component inside it</h2>
-        </>
-    );
+  const [visibleSection, setVisibleSection] = useState("about");
+  return (
+    <>
+      <Section
+        title={"About Instamart"}
+        description={
+          "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."
+        }
+        show={visibleSection === "about"}
+        setShow={(check) => {
+          check ? setVisibleSection("about") : setVisibleSection("");
+        }}
+      />
+      <Section
+        title={"Careers at Instamart"}
+        description={
+          "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."
+        }
+        show={visibleSection === "careers"}
+        setShow={(check) => {
+          check ? setVisibleSection("careers") : setVisibleSection("");
+        }}
+      />
+      <Section
+        title={"Products of Instamart"}
+        description={
+          "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."
+        }
+        show={visibleSection === "products"}
+        setShow={(check) => {
+          check ? setVisibleSection("products") : setVisibleSection("");
+        }}
+      />
+    </>
+  );
 };
 export default Instamart;
