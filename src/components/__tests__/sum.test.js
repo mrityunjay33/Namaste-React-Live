6aea643bd67d91a17b2dcc8f96c5b59b98d0f0fc
Mrityunjay Kumar <mk10399@gmail.com>
2023-03-06 01:38:09 +0530

    testing configuration

---
+++ src/components/__tests__/sum.test.js
@@ -0,0 +1,41 @@
+const sum = (a, b) => {
+    return a + b;
+  }
+  export default sum;
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+/**
+ *
+ *  Different Type of Testing??
+ *  - Manual Testing
+ *  - Autmation Testing
+ *      - Selenium Testing
+ *
+ *
+ *  - E2E Testing - Covers entire user Journey
+ *
+ *  - Unit Testing
+ *  - Integration Testing
+ *
+ *
+ *
+ *
+ * - Install React Testing Librabry
+ * - Install Jest
+ * - configure Jest
+ * - Installed jest-environment-jsdom
+ * - Create my first test
+ * - Configure babel
+ * - Wrote expect sum test
+ * - gitignore coverage report
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
