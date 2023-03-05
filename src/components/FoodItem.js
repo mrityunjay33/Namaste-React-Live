0c357af008336e39b0ccdcf48aa7b57d1c17875e
Mrityunjay Kumar <mk10399@gmail.com>
2023-03-05 19:17:00 +0530

    default image on no img with cdn

--- src/components/FoodItem.js
+++ src/components/FoodItem.js
@@ -1,13 +1,22 @@
 import { IMG_CDN_URL } from "../contants";
+import Image from "../assets/img/foodvilla.png";
 
 const FoodItem = ({ name, description, cloudinaryImageId, price }) => {
   return (
     <>
       <div className="w-56 p-2 m-2 shadow-lg bg-pink-50">
-        <img src={`${IMG_CDN_URL}${cloudinaryImageId ? cloudinaryImageId : ""}`} alt={description} onError="this.src='https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/zmxq0pzd6sqtzm1ibpj2'" />
+        <img
+          src={
+            cloudinaryImageId.length > 1
+              ? IMG_CDN_URL + cloudinaryImageId
+              : Image
+          }
+          alt={description}
+          onError={() => ""}
+        />
         <h2 className="font-bold text-xl">{name}</h2>
         <div className="font-medium">{description}</div>
-        <div>Rupees: {price}</div>
+        <div>Rupees: {price/100}</div>
       </div>
     </>
   );
