241d31d9adc8f7ee3f03eb18d10c096d3d29d736
Mrityunjay Kumar <mk10399@gmail.com>
2023-03-06 02:23:18 +0530

    First testing of logo loading

--- src/components/__tests__/Header.test.js
+++ src/components/__tests__/Header.test.js
@@ -1,11 +1,18 @@
 import Header from '../Header';
-import { render } from '@testing-library/react';
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import store from '../../utils/Store';
+import { StaticRouter } from'react-router-dom/server';
 
 test("Logo should render on loading", () => {
     const header = render(
+        <StaticRouter>
+        <Provider store={store}>
         <Header />
+        </Provider>
+        </StaticRouter>
     );
 
     const logo = header.getAllByTestId('logo');
-    console.log(logo);
+    expect(logo[0].src).toBe("http://localhost/dummy.png");
 });
